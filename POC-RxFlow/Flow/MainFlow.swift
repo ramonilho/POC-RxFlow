b4d37a940d1c68ae6b55f266586afa600b579ed0
Ramon Honorio <ramon@Ramons-MacBook-Pro.local>
2018-12-04 00:02:43 -0200

    cartfile added

--- POC-RxFlow/Flow/MainFlow.swift
+++ POC-RxFlow/Flow/MainFlow.swift
@@ -1,5 +1,5 @@
 //
-//  HomeFlow.swift
+//  MainFlow.swift
 //  POC-RxFlow
 //
 //  Created by Ramon Honorio on 03/12/18.
@@ -16,15 +16,37 @@ class MainFlow: Flow {
     }
     
     func navigate(to step: Step) -> NextFlowItems {
-        return .none
+        guard let step = step as? DashboardStep else { return NextFlowItems.none }
+        switch step {
+        case .initial:
+            return navigationToDashboard()
+        default:
+            return .none
+        }
     }
-
-    private lazy var rootViewController: ViewController = {
-        return ViewController()
-    }()
     
-    deinit {
-        print("\(type(of: self)): \(#function)")
+    let rootViewController = UITabBarController()
+
+    private func navigationToDashboard() -> NextFlowItems {
+        let redFlow = RedFlow()
+        let greenFlow = GreenFlow()
+        
+        Flows.whenReady(flow1: redFlow, flow2: greenFlow) { [unowned self] (rootRed: UINavigationController, rootGreen: UINavigationController) in
+            
+            let redTabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 0)
+            rootRed.tabBarItem = redTabBarItem
+            
+            let greenTabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+            rootGreen.tabBarItem = greenTabBarItem
+            
+            self.rootViewController.setViewControllers([rootRed, rootGreen], animated: false)
+        }
+        
+        return .multiple(flowItems: [
+            NextFlowItem(nextPresentable: redFlow, nextStepper: RedStepper()),
+            NextFlowItem(nextPresentable: greenFlow, nextStepper: GreenStepper()),
+            ]
+        )
     }
     
 }
